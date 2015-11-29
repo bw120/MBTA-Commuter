4b2266d933939667b78f8eab6791609382954e1b
bw120 <benwright120@yahoo.com>
2015-11-29 15:36:42 -0500

    Feat: added function to Mbta service to get stops for a particular route from the MBTA API.

--- js/controllers.js
+++ js/controllers.js
@@ -3,24 +3,48 @@ function MenuCtrl ($scope) {
 
 }
 
-
 function BuilderCtrl ($scope, myData, myCommutes, Mbta) {
 	var self = this;
 	$scope.Data = myData;
 	$scope.myCommutes = myCommutes;
 
+	var commutes = [{
+		name: "",
+		routeLeg: [{
+			line: "",
+			direction: "",
+			boardingStop: "",
+			disboardStop: ""
+		}],
 
+	}];
+
+
+	$scope.selectedLine;
+
+	//gets a list of all routes for all modes of transit
 	Mbta.getRoutes()
 		.then(function(data) {
 		    $scope.AllRoutes = data;
 		    console.log(data);
-	})
+	});
+
+	//gets a list of stops for the selected route
+	$scope.getStops = function(route) {
+		console.log(route);
+		Mbta.getStops(route)
+			.then(function(data) {
+			    $scope.Stops = data;
+			    $scope.selectedLine = route;
+			});
+	}
 
 
 }
 
 function DashboardCtrl ($scope, myData) {
-	$scope.Data = myData;
+	$scope.myCommutes = myCommutes;
+
 }
 
 
