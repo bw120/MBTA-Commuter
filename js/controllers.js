8b5657bd721cd4703ac49fed89c1fe673a55f3e2
Ben Wright <benwright120@yahoo.com>
2016-02-20 17:21:16 -0500

    Feat: updated the prediction functions to filter only the trips scheduled to go to the destination stop.

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-function BuilderCtrl ($scope, $location, myCommutes, Mbta) {
+function BuilderCtrl($scope, $location, myCommutes, Mbta) {
 	var self = this;
 	$scope.allCommutes = myCommutes.getData();
 
@@ -14,17 +14,16 @@ function BuilderCtrl ($scope, $location, myCommutes, Mbta) {
 	//gets a list of all routes for all modes of transit
 	Mbta.getRoutes()
 		.then(function(data) {
-		    $scope.AllRoutes = data;
-	});
+			$scope.AllRoutes = data;
+		});
 
 	//gets a list of stops for the selected route
 	$scope.getStops = function(route) {
 		Mbta.getStops(route.route_id)
 			.then(function(data) {
-			    $scope.Stops = data;
-			    $scope.selectedLine = route.route_id;
-			    $scope.line = route.route_name;
-			    
+				$scope.Stops = data;
+				$scope.selectedLine = route.route_id;
+				$scope.line = route.route_name;
 			});
 	};
 
@@ -37,11 +36,12 @@ function BuilderCtrl ($scope, $location, myCommutes, Mbta) {
 			line: $scope.leg.selectedLine.route_name,
 			direction: $scope.leg.direction.direction_name,
 			boardingStopID: $scope.leg.boarding.stop_id,
+			boardingStopParent: $scope.leg.boarding.parent_station,
 			disboardStopID: $scope.leg.deboarding.stop_id,
+			disboardStopParent: $scope.leg.deboarding.parent_station,
 			boardingStop: $scope.leg.boarding.stop_name,
 			disboardStop: $scope.leg.deboarding.stop_name
 		};
-
 		$scope.commute.routeLegs.push(leg);
 		$scope.leg = null;
 		$scope.commute.edit = 1;
@@ -55,44 +55,40 @@ function BuilderCtrl ($scope, $location, myCommutes, Mbta) {
 
 }
 
-function DashboardCtrl ($scope, $location, $interval, $route, $rootScope, myCommutes, Mbta, $rootScope) {
+function DashboardCtrl($scope, $location, $interval, $route, $rootScope, myCommutes, Mbta) {
 	$scope.allCommutes = myCommutes.getData();
 	$scope.showAlert = [];
-	$scope.allAlerts ={};
-	$scope.allPredictions =[];
+	$scope.allAlerts = {};
+	$scope.allPredictions = [];
 	$scope.predicate = "pre_away";
 	var theUpdates;
 
 	//When logging out destroy Firebase reference.
-	$rootScope.$on("logout", function(){
+	$rootScope.$on("logout", function() {
 		$scope.allCommutes.$destroy();
 	});
 
 	//Makes a request to get alerts for each line
-	var updateAlerts = function () {
+	var updateAlerts = function() {
 		for (x = 0; x < $scope.allCommutes.length; x++) {
 			for (y = 0; y < $scope.allCommutes[x].routeLegs.length; y++) {
 				Mbta.getAlerts($scope.allCommutes[x].routeLegs[y].lineID)
-						.then(function(data) {
-						    $scope.allAlerts[data.data.route_id] = data.data.alerts;
-						});
+					.then(function(data) {
+						$scope.allAlerts[data.data.route_id] = data.data.alerts;
+					});
 			}
 		}
 	};
 
 	//Makes a request to get predictions for each line
-	var updatePredictions = function () {
+	var updatePredictions = function() {
 		for (x = 0; x < $scope.allCommutes.length; x++) {
 			for (y = 0; y < $scope.allCommutes[x].routeLegs.length; y++) {
-				Mbta.getArrivals($scope.allCommutes[x].routeLegs[y].lineID, $scope.allCommutes[x].routeLegs[y].boardingStopID, $scope.allCommutes[x].routeLegs[y].direction)
-						.then(function(data) {		
-							var predictions = data.predictions;
-							if (predictions) {
-								$scope.allPredictions[data.id] = predictions.sort(function(a, b) {
-								    return parseInt(a.pre_away) - parseInt(b.pre_away);
-								});
-							}
-						});
+				Mbta.getArrivals($scope.allCommutes[x].routeLegs[y].lineID, $scope.allCommutes[x].routeLegs[y].boardingStopID,
+						$scope.allCommutes[x].routeLegs[y].disboardStopID, $scope.allCommutes[x].routeLegs[y].direction, $scope.allCommutes[x].$id)
+					.then(function(data) {
+						$scope.allPredictions[data.id] = data;
+					});
 			}
 		}
 	};
@@ -111,18 +107,18 @@ function DashboardCtrl ($scope, $location, $interval, $route, $rootScope, myComm
 				console.log("updated alerts and predicitons");
 			}, 11000);
 
-		});	
+		});
 	//cancel the interval requests for updates on arrival predictions and alerts
-  	$rootScope.$on("$routeChangeSuccess", 
-        function (event, current, previous, rejection) {
-        if (angular.isDefined(theUpdates)) {
-	        $interval.cancel(theUpdates);
-	        theUpdates = undefined;
-    	}
-  });
+	$rootScope.$on("$routeChangeSuccess",
+		function(event, current, previous, rejection) {
+			if (angular.isDefined(theUpdates)) {
+				$interval.cancel(theUpdates);
+				theUpdates = undefined;
+			}
+		});
 }
 
-function MenuCtrl ($scope, Auth, $location) {
+function MenuCtrl($scope, Auth, $location) {
 	$scope.menuOpen = false;
 	$scope.showMenu = false;
 
@@ -131,59 +127,58 @@ function MenuCtrl ($scope, Auth, $location) {
 		$location.path('/login');
 	};
 
-};
-
-function AccountCtrl ($scope, $location, Auth) {
-	$scope.logError = null;
-    $scope.auth = Auth.auth;
-
-    $scope.auth.$onAuth(function(authData) {
-    	if (Auth.getAuthState() != null) {
-			$scope.email = Auth.getAuthState().password.email;
-		}
-    });
-
-	$scope.changePass = function() {
-		console.log($scope.OldPassword != undefined);
-		if ($scope.newPassword === $scope.confirmPassword && $scope.OldPassword != undefined && $scope.newPassword != undefined && $scope.confirmPassword != undefined ) {
-			$scope.message = Auth.updatePass($scope.email, $scope.OldPassword, $scope.newPassword).then(function (message) {
-	    		$scope.logError = message;
-	    	});
-		} else if ($scope.OldPassword === undefined || $scope.newPassword === undefined || $scope.confirmPassword === undefined ){
-			$scope.logError = "One of the required fields is empty!";
-		} else {
-			$scope.logError = "Please re-type your new password. It does not match!"
-		}
-	}
 }
 
-function LoginCtrl ($scope, $location, Auth) {
+function AccountCtrl($scope, $location, Auth) {
+	$scope.logError = null;
+	$scope.auth = Auth.auth;
+
+	$scope.auth.$onAuth(function(authData) {
+		if (Auth.getAuthState() !== null) {
+			$scope.email = Auth.getAuthState().password.email;
+		}
+	});
+
+	$scope.changePass = function() {
+		if ($scope.newPassword === $scope.confirmPassword && $scope.OldPassword !== undefined && $scope.newPassword !== undefined && $scope.confirmPassword !== undefined) {
+			$scope.message = Auth.updatePass($scope.email, $scope.OldPassword, $scope.newPassword).then(function(message) {
+				$scope.logError = message;
+			});
+		} else if ($scope.OldPassword === undefined || $scope.newPassword === undefined || $scope.confirmPassword === undefined) {
+			$scope.logError = "One of the required fields is empty!";
+		} else {
+			$scope.logError = "Please re-type your new password. It does not match!";
+		}
+	};
+}
+
+function LoginCtrl($scope, $location, Auth) {
 
 	$scope.whichToShow = 1;
 	$scope.logError = null;
 
 	$scope.createUser = function() {
 		if ($scope.password === $scope.confirmPassword) {
-	    	Auth.createUser($scope.email, $scope.password).then(function (message) {
-	    		$scope.whichToShow = 1;
-	    		$scope.logError = message;
+			Auth.createUser($scope.email, $scope.password).then(function(message) {
+				$scope.whichToShow = 1;
+				$scope.logError = message;
 
-	    	});   
+			});
 		} else {
-			$scope.logError = "Please re-type your new password. It does not match!"
-		} 		
-    };
+			$scope.logError = "Please re-type your new password. It does not match!";
+		}
+	};
 
-    $scope.userLogin = function() {
-    	var message = Auth.login($scope.email, $scope.password).then(function (message) {
-    		$scope.logError = message;
-    	});
+	$scope.userLogin = function() {
+		var message = Auth.login($scope.email, $scope.password).then(function(message) {
+			$scope.logError = message;
+		});
 
-    };
+	};
 
-    $scope.passReset = function() {
-    	Auth.resetPass($scope.email).then(function (message) {
-    		$scope.logError = message;
-    	});
-    }
+	$scope.passReset = function() {
+		Auth.resetPass($scope.email).then(function(message) {
+			$scope.logError = message;
+		});
+	};
 }
--- js/services.js
+++ js/services.js
@@ -1,7 +1,7 @@
 //factory to control user authentication
 //TODO: create function to for forgotten password
 //		Creat function to update email
-function Auth ($firebaseAuth, $firebaseArray, $location, $rootScope) {
+function Auth($firebaseAuth, $firebaseArray, $location, $rootScope) {
 	var ref = new Firebase("https://tcommutes.firebaseio.com");
 
 	var fb = {
@@ -9,75 +9,75 @@ function Auth ($firebaseAuth, $firebaseArray, $location, $rootScope) {
 		password: "",
 		auth: $firebaseAuth(ref),
 		login: function(usr, pass) {
-	    	
-	    	this.username = usr;
-	    	this.password = pass;
-	    
-	    
-	    	return this.auth.$authWithPassword({
-		    	email: this.username,
-		        password: this.password
-	     	}).then(function (authData) {
 
-	     		this.authData = authData;
-	     		$location.path('/dashboard');
+			this.username = usr;
+			this.password = pass;
 
-	     	}).catch(function(error) {
-	     		var error = "Either the username or password are not correct.";
-	     		return error;
-	     	});
 
-    	},
-    	logout: function() {
-    		this.auth.$unauth();
-    		$rootScope.$broadcast('logout');
-    	},
-    	getAuthState: function() {
-    		var authData = this.auth.$getAuth();
-    		return authData;
+			return this.auth.$authWithPassword({
+				email: this.username,
+				password: this.password
+			}).then(function(authData) {
 
-    	},
-    	createUser: function(usr, pass) {
+				this.authData = authData;
+				$location.path('/dashboard');
+
+			}).catch(function(error) {
+				var message = "Either the username or password are not correct.";
+				return message;
+			});
+
+		},
+		logout: function() {
+			this.auth.$unauth();
+			$rootScope.$broadcast('logout');
+		},
+		getAuthState: function() {
+			var authData = this.auth.$getAuth();
+			return authData;
+
+		},
+		createUser: function(usr, pass) {
 
 			return this.auth.$createUser({
-		        email: usr,
-		        password: pass
-		    }).then(function(userData) {
-		    	console.log("User created with uid: " + userData.uid);
-		    	return "Account created! Please login using your email and password";
-		    }).catch(function(error) {
-		    	console.log(error);
-		    	return "Sorry, an error occured. We were not able to set up an account";
-		    });
-	    },
-	    resetPass: function(email) {
-	    	return this.auth.$resetPassword({
-			  	email: email
-			}).then(function() {
-				var message = "Password reset email sent successfully!"
-			  console.log(message);
-			  return message;
+				email: usr,
+				password: pass
+			}).then(function(userData) {
+				console.log("User created with uid: " + userData.uid);
+				return "Account created! Please login using your email and password";
 			}).catch(function(error) {
-			  console.log(error);
-			  return "Error: Check that your email was entered correctly";
+				console.log(error);
+				return "Sorry, an error occured. We were not able to set up an account";
 			});
-	    },
-	    updatePass: function(email, oldEmail, newEmail) {
+		},
+		resetPass: function(email) {
+			return this.auth.$resetPassword({
+				email: email
+			}).then(function() {
+				var message = "Password reset email sent successfully!";
+				console.log(message);
+				return message;
+			}).catch(function(error) {
+				console.log(error);
+				return "Error: Check that your email was entered correctly";
+			});
+		},
+		updatePass: function(email, oldEmail, newEmail) {
 
-	    	return this.auth.$changePassword({
-			  email: email,
-			  oldPassword: oldEmail,
-			  newPassword: newEmail
+			return this.auth.$changePassword({
+				email: email,
+				oldPassword: oldEmail,
+				newPassword: newEmail
 			}).then(function() {
-			  console.log("Password changed successfully!");
-			  return "Password changed successfully!"
+				console.log("Password changed successfully!");
+				return "Password changed successfully!";
 			}).catch(function(error) {
-			  var message = "" + error;
-			  return message;
+				var message = "" + error;
+				return message;
 			});
-	    }
-	        	
-    }
+		}
+
+	};
 
 	return fb;
 
@@ -85,10 +85,10 @@ function Auth ($firebaseAuth, $firebaseArray, $location, $rootScope) {
 
 
 //syncs user data with Firebase
-function myCommutes ($firebaseArray, Auth) {
+function myCommutes($firebaseArray, Auth) {
 
 	var commutes = {
-		getData: function () {
+		getData: function() {
 			if (Auth.getAuthState().uid) {
 				var user = Auth.getAuthState().uid;
 				var ref = new Firebase("https://tcommutes.firebaseio.com/").child("commutes").child(user);
@@ -101,20 +101,20 @@ function myCommutes ($firebaseArray, Auth) {
 		}
 
 	};
-    return commutes;
-};
+	return commutes;
+}
 
 
 //loads various info from MBTA API
-function Mbta ($http, $q) {
+function Mbta($http, $q) {
 	var apiURL = function(query) {
-		
+
 		return "http://realtime.mbta.com/developer/api/v2/" + query + "api_key=wX9NwuHnZU2ToO7GmGR9uw&format=json"; //developement api key
 		//return "http://realtime.mbta.com/developer/api/v2/" + query + "api_key=ed9Jx40ToEWg1VNZqWyYaw&format=json"; // production api Key
-	}
+	};
 
 	this.getRoutes = function() {
-		var query = "routes?"
+		var query = "routes?";
 		return $http.get(apiURL(query))
 			.then(
 				function(res) {
@@ -125,26 +125,26 @@ function Mbta ($http, $q) {
 					routes[2] = res.data.mode[3].route;
 					routes[3] = res.data.mode[4].route;
 					return routes;
-			},
-			function(res) {
-				console.log("error!");
-				return res;
-			});
+				},
+				function(res) {
+					console.log("error!");
+					return res;
+				});
 
-	}
+	};
 	this.getStops = function(route) {
 		var query = "stopsbyroute?route=" + route + "&";
 		return $http.get(apiURL(query))
 			.then(
 				function(res) {
 					return res.data.direction;
-			},
-			function(res) {
-				console.log("error!");
-				return res;
-			});
+				},
+				function(res) {
+					console.log("error!");
+					return res;
+				});
 
-	}
+	};
 
 	//gets service alerts for requested route
 	this.getAlerts = function(routes) {
@@ -152,58 +152,86 @@ function Mbta ($http, $q) {
 		return $http.get(apiURL(query))
 			.then(
 				function(res) {
-					
 					return res;
-			},
-			function(res) {
-				console.log("error!");
-				return res;
-			});
-	}
+				},
+				function(res) {
+					console.log("error!");
+					return res;
+				});
+	};
+
 
 	//get service arrival prediction for each route
-	this.getArrivals = function(route, stop, direction) {
-		var query = "predictionsbystop?stop=" + stop + "&";
-		var id = route + "-" + stop + "-" + direction;
-		return $http.get(apiURL(query))
-			.then(
-				function(res) {
-					var predictions = {};
-					// var id = route + "-" + stop + "-" + direction;
-					if (res.data.mode) {
-						for (x = 0; x < res.data.mode.length; x++) {
-							for (y = 0; y < res.data.mode[x].route.length; y++) {
-								if (res.data.mode[x].route[y].route_id == route) {
-									for (z = 0; z < res.data.mode[x].route[y].direction.length; z++) {
-										if (res.data.mode[x].route[y].direction[z].direction_name == direction) {
-											predictions = {
-															"id": id,
-															"route": route,
-															"stop" : stop,
-															"direction": direction,
-															"predictions": predictions.trips = res.data.mode[x].route[y].direction[z].trip};
+	//get scheduled trips to destination stop. Filter predictions for trips going to destination stop
+	this.getArrivals = function(route, boardstop, exitStop, direction, id) {
+		var query1 = "predictionsbystop?stop=" + boardstop + "&";
+		//this request pulls predictions from the MBTA Api
+		var request1 = $http.get(apiURL(query1));
+		//this pulls from pre-configured json files for list of scheduled trips gong to the destination stop
+		var request2 = $http.get("../json/stops/" + exitStop + ".json");
 
-										}
-									}
+		//sorts data and pull out predictions just for the selected route and direction
+		var sortPredictions = function(data) {
+			var predictions = {};
+			if (data.mode) {
+				for (x = 0; x < data.mode.length; x++) {
+					for (y = 0; y < data.mode[x].route.length; y++) {
+						if (data.mode[x].route[y].route_id == route) {
+							for (z = 0; z < data.mode[x].route[y].direction.length; z++) {
+								if (data.mode[x].route[y].direction[z].direction_name == direction) {
+									predictions = data.mode[x].route[y].direction[z].trip;
+
 								}
 							}
 						}
-						
-					} else {
-						predictions = {
-										"id": id,
-										"route": route,
-										"stop" : stop,
-										"direction": direction,
-										"predictions": []};
 					}
-					return predictions;
+				}
+
+			} else {
+				predictions = [];
+
+			}
+			return predictions;
+		};
+
+		//fire off two request to the MBTA API
+		//once the data comes back from both, filter on the tripID's that will go to the destination stop
+		return $q.all([request1, request2]).then(function(data) {
+				var predictArray = [];
+				var predictData = sortPredictions(data[0].data);
+				var predictions = predictData;
+				var destIDs = data[1].data[exitStop];
+
+				//add to array only those who are scheduled to go to the destination stop
+				if (predictData && predictData.length > 0) {
+					for (var t = 0; t < predictData.length; t++) {
+						for (var u = 0; u < destIDs.length; u++) {
+							if (predictData[t].trip_id === destIDs[u]) {
+								predictArray.push(predictData[t]);
+							}
+						}
+					}
+				}
+
+				//sort predictions in order of arrival time
+				if (predictArray.length > 0) {
+					predictArray = predictArray.sort(function(a, b) {
+						return parseInt(a.pre_away) - parseInt(b.pre_away);
+					});
+				}
+
+				return {
+					"id": id,
+					"route": route,
+					"stop": stop,
+					"direction": direction,
+					"predictions": predictArray
+				};
+
 			},
 			function(res) {
 				console.log("error!");
 				return res;
 			});
-	}
-
-
+	};
 }
